f3f64ad2c9f38ca43fa32194ce3baa1217c0e500
Salim Emmanuel Raji <salimraji@icloud.com>
2025-01-18 13:36:19 +0200

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    // Function to handle button click and navigate to newpage.html
+    function goToNewPage1() {
+        window.location.href = "positive.html";
+    }
+
+    // Add click event listener to the button
+    document.getElementById("positiveID").addEventListener("click", goToNewPage1);
+
+        // Function to handle button click and navigate to newpage.html
+    function goToNewPage2() {
+        window.location.href = "negative.html";
+    }
+    
+        // Add click event listener to the button
+    document.getElementById("negativeID").addEventListener("click", goToNewPage2);
